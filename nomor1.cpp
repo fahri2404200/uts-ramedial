d308e4b8b0849d319909979ef28a9eded4feab67
iinmahgafhira07 <49119136+iinmahgafhira07@users.noreply.github.com>
2019-03-31 22:03:47 -0700

    Create nomor1.cpp

---
+++ nomor1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+   cout << "nama          : iin mahgafhira"<<endl;
+   cout << "nim           : 1829140022"<<endl;
+   cout << "kelas         : teknik komputer B"<<endl;
+   cout << "tahun angkatan: 2018"<<endl;
+   
+return 0;
+}
