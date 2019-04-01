fdf1bf95105db985e8570f159b7aed82b691524b
iinmahgafhira07 <49119136+iinmahgafhira07@users.noreply.github.com>
2019-03-31 22:17:22 -0700

    Create nomor2.cpp

---
+++ nomor2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct data_mhs
+{
+   string nama;
+   int nim;
+   float ipk;
+} a,b;
+
+int main()
+{
+   a.nama = "ajis gagap";
+   a.nim = 102904049;
+   a.ipk = 2.5;
+   b.nama = tumming";
+   b.nim = 102904050;
+   b.ipk =  3.0;
+   
+   cout << "nama : " << a.nama <<endl;
+   cout << "nim  : " << a.nim <<endl;
+   cout << "ipk  : " << a.ipk <<endl;
+   cout <<"\n";
+   cout << "nama : " << b.nama <<endl;
+   cout << "nim  : " << b.nim <<endl;
+   cout << "ipk  : " << b.ipk <<endl;
+   
+return 0;
